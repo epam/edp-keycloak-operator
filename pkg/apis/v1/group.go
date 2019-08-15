4b06718b6c6858fea21ad7aadd51cd2ed3628d75
Pavlo_Yemelianov <pavlo_yemelianov@epam.com>
2019-08-15 17:05:59 +0300

    [EPMDEDP-3549] Add keycloak realm controller
    Change-Id: Idcb551519f8dd198287313a5cf53ef991fa9a9ee

---
+++ pkg/apis/v1/group.go
@@ -0,0 +1,6 @@
+// Package v1 contains v1 API versions.
+//
+// This file ensures Go source parsers acknowledge the v1 package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package v1
---
+++ pkg/controller/keycloakrealm/keycloakrealm_controller.go
@@ -0,0 +1,83 @@
+package keycloakrealm
+
+import (
+	"context"
+
+	v1v1alpha1 "keycloak-operator/pkg/apis/v1/v1alpha1"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+var log = logf.Log.WithName("controller_keycloakrealm")
+
+/**
+* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
+* business logic.  Delete these comments after modifying this file.*
+ */
+
+// Add creates a new KeycloakRealm Controller and adds it to the Manager. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &ReconcileKeycloakRealm{client: mgr.GetClient(), scheme: mgr.GetScheme()}
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New("keycloakrealm-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to primary resource KeycloakRealm
+	return c.Watch(&source.Kind{Type: &v1v1alpha1.KeycloakRealm{}}, &handler.EnqueueRequestForObject{})
+}
+
+// blank assignment to verify that ReconcileKeycloakRealm implements reconcile.Reconciler
+var _ reconcile.Reconciler = &ReconcileKeycloakRealm{}
+
+// ReconcileKeycloakRealm reconciles a KeycloakRealm object
+type ReconcileKeycloakRealm struct {
+	// This client, initialized using mgr.Client() above, is a split client
+	// that reads objects from the cache and writes to the apiserver
+	client client.Client
+	scheme *runtime.Scheme
+}
+
+// Reconcile reads that state of the cluster for a KeycloakRealm object and makes changes based on the state read
+// and what is in the KeycloakRealm.Spec
+// The Controller will requeue the Request to be processed again if the returned error is non-nil or
+// Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
+func (r *ReconcileKeycloakRealm) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
+	reqLogger.Info("Reconciling KeycloakRealm")
+
+	// Fetch the KeycloakRealm instance
+	instance := &v1v1alpha1.KeycloakRealm{}
+	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+
+	return reconcile.Result{}, nil
+}
