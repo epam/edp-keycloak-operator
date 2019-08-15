f9739dd102184dd968f9ff92b306297a095850cf
Pavlo_Yemelianov <pavlo_yemelianov@epam.com>
2019-08-15 17:05:59 +0300

    [EPMDEDP-3549] Add keycloak realm controller
    Change-Id: Idcb551519f8dd198287313a5cf53ef991fa9a9ee

---
+++ pkg/apis/addtoscheme_v1_v1alpha1.go
@@ -0,0 +1,10 @@
+package apis
+
+import (
+	"keycloak-operator/pkg/apis/v1/v1alpha1"
+)
+
+func init() {
+	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
+	AddToSchemes = append(AddToSchemes, v1alpha1.SchemeBuilder.AddToScheme)
+}
---
+++ pkg/apis/v1/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the v1 v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=v1.edp.epam.com
+package v1alpha1
