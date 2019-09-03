0359349d93c54fc1400e90752c7239006e514155
Pavlo_Yemelianov <pavlo_yemelianov@epam.com>
2019-09-03 14:59:29 +0300

    [EPMDEDP-3564] Create Keycloak Client CR for integration with Openshift realm
    Change-Id: I5a986a2baedcb1d978b42b9c0ff73ecfcbca4f08

--- pkg/controller/keycloakrealm/keycloakrealm_controller.go
+++ pkg/controller/keycloakrealm/keycloakrealm_controller.go
@@ -2,16 +2,19 @@ package keycloakrealm
 
 import (
 	"context"
+	"github.com/google/uuid"
 	coreerrors "github.com/pkg/errors"
 	"gopkg.in/nerzal/gocloak.v2"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"keycloak-operator/pkg/adapter/keycloak"
 	v1v1alpha1 "keycloak-operator/pkg/apis/v1/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -92,6 +95,7 @@ func (r *ReconcileKeycloakRealm) Reconcile(request reconcile.Request) (reconcile
 	}
 
 	err = r.tryReconcile(instance)
+	instance.Status.Available = err == nil
 
 	_ = r.client.Update(context.TODO(), instance)
 
@@ -101,16 +105,20 @@ func (r *ReconcileKeycloakRealm) Reconcile(request reconcile.Request) (reconcile
 func (r *ReconcileKeycloakRealm) tryReconcile(realm *v1v1alpha1.KeycloakRealm) error {
 	ownerKeycloak, err := r.getOwnerKeycloak(realm)
 	if err != nil {
-		realm.Status.Available = false
 		return err
 	}
 
 	if !ownerKeycloak.Status.Connected {
-		realm.Status.Available = false
-		return nil
+		return coreerrors.New("Owner keycloak is not in connected status")
 	}
 	err = r.putRealm(ownerKeycloak, realm)
-	realm.Status.Available = err == nil
+
+	if err != nil {
+		return err
+	}
+
+	err = r.putKeycloakClientCR(realm)
+
 	return err
 }
 
@@ -171,6 +179,66 @@ func (r *ReconcileKeycloakRealm) getOwnerKeycloak(realm *v1v1alpha1.KeycloakReal
 	ownerCr := &v1v1alpha1.Keycloak{}
 	err := r.client.Get(context.TODO(), nsn, ownerCr)
 	return ownerCr, err
+}
+
+func (r *ReconcileKeycloakRealm) putKeycloakClientCR(realm *v1v1alpha1.KeycloakRealm) error {
+	reqLog := log.WithValues("realm name", realm.Spec.RealmName)
+	reqLog.Info("Start creation of Keycloak client CR")
+
+	instance, err := r.getKeycloakClientCR(realm.Spec.RealmName, realm.Namespace)
+
+	if err != nil {
+		return err
+	}
+	if instance != nil {
+		reqLog.Info("Required Keycloak client CR already exists")
+		return nil
+	}
+
+	secret := uuid.New().String()
+	instance = &v1v1alpha1.KeycloakClient{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      realm.Spec.RealmName,
+			Namespace: realm.Namespace,
+		},
+		Spec: v1v1alpha1.KeycloakClientSpec{
+			ClientId:     realm.Spec.RealmName,
+			ClientSecret: secret,
+			TargetRealm:  "openshift",
+		},
+	}
+	err = controllerutil.SetControllerReference(realm, instance, r.scheme)
+	if err != nil {
+		return coreerrors.Wrap(err, "cannot set owner ref for keycloak client CR")
+	}
+	err = r.client.Create(context.TODO(), instance)
+	if err != nil {
+		return coreerrors.Wrap(err, "cannot create keycloak client cr")
+	}
+	reqLog.Info("Keycloak client has been successfully created", "keycloak client", instance)
+
+	return nil
+}
+
+func (r *ReconcileKeycloakRealm) getKeycloakClientCR(name, namespace string) (*v1v1alpha1.KeycloakClient, error) {
+	reqLog := log.WithValues("keycloak client name", name, "keycloak client namespace", namespace)
+	reqLog.Info("Start retrieve keycloak client cr...")
+
+	instance := &v1v1alpha1.KeycloakClient{}
+	nsn := types.NamespacedName{
+		Name:      name,
+		Namespace: namespace,
+	}
+	err := r.client.Get(context.TODO(), nsn, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			reqLog.Info("Keycloak Client CR has not been found")
+			return nil, nil
+		}
+		return nil, coreerrors.Wrap(err, "cannot read keycloak client CR")
+	}
+	reqLog.Info("Keycloak client has bee retrieved", "keycloak client cr", instance)
+	return instance, nil
 }
 
 func getKeycloakOwner(references []v1.OwnerReference) *v1.OwnerReference {
