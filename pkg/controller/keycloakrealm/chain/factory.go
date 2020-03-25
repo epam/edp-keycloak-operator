19e39ef3e0a48bdceb5baf5f8fd32dc6700fb800
Anton_Tarianyk <Anton_Tarianyk@epam.com>
2020-03-25 15:40:37 +0200

    [EPMDEDP-4825] Create Client Scope as separate step
    Change-Id: I142462f14665b305c30371c080807aba6e7e285c

--- pkg/controller/keycloakrealm/chain/factory.go
+++ pkg/controller/keycloakrealm/chain/factory.go
@@ -14,24 +14,27 @@ var log = logf.Log.WithName("realm_handler")
 func CreateDefChain(client client.Client, scheme *runtime.Scheme) handler.RealmHandler {
 	return PutAcSecret{
 		next: PutRealm{
-			next: PutKeycloakClientCR{
-				next: PutKeycloakClientSecret{
-					next: PutUsers{
-						next: PutUsersRoles{
-							next: PutOpenIdConfigAnnotation{
-								next: PutIdentityProvider{
-									next:   PutDefaultIdP{},
+			next: PutKeycloakClientScope{
+				next: PutKeycloakClientCR{
+					next: PutKeycloakClientSecret{
+						next: PutUsers{
+							next: PutUsersRoles{
+								next: PutOpenIdConfigAnnotation{
+									next: PutIdentityProvider{
+										next:   PutDefaultIdP{},
+										client: client,
+									},
 									client: client,
 								},
-								client: client,
 							},
 						},
+						client: client,
+						scheme: scheme,
 					},
 					client: client,
 					scheme: scheme,
 				},
 				client: client,
-				scheme: scheme,
 			},
 			client: client,
 		},
---
+++ pkg/controller/keycloakrealm/chain/keycloak_client_scope.go
@@ -0,0 +1,41 @@
+package chain
+
+import (
+	"github.com/epmd-edp/keycloak-operator/pkg/apis/v1/v1alpha1"
+	"github.com/epmd-edp/keycloak-operator/pkg/client/keycloak"
+	"github.com/epmd-edp/keycloak-operator/pkg/consts"
+	"github.com/epmd-edp/keycloak-operator/pkg/controller/keycloakrealm/chain/handler"
+	"github.com/epmd-edp/keycloak-operator/pkg/model"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type PutKeycloakClientScope struct {
+	next   handler.RealmHandler
+	client client.Client
+	scheme *runtime.Scheme
+}
+
+func (h PutKeycloakClientScope) ServeRequest(realm *v1alpha1.KeycloakRealm, kClient keycloak.Client) error {
+	rLog := log.WithValues("realm name", realm.Spec.RealmName)
+	rLog.Info("Start Keycloak Scope")
+	if err := kClient.CreateClientScope(realm.Spec.RealmName, getDefClientScope()); err != nil {
+		return err
+	}
+	rLog.Info("End of put Keycloak Scope")
+	return nextServeOrNil(h.next, realm, kClient)
+}
+
+func getDefClientScope() model.ClientScope {
+	return model.ClientScope{
+		Name:        stringP(consts.DefaultClientScopeName),
+		Description: stringP("default edp scope required for ac and nexus"),
+		Protocol:    stringP(consts.OpenIdProtocol),
+		ClientScopeAttributes: &model.ClientScopeAttributes{
+			IncludeInTokenScope: stringP("true"),
+		},
+	}
+}
+func stringP(value string) *string {
+	return &value
+}
