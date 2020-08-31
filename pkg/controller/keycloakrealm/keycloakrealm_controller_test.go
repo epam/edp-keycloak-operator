f0768a3f5a57a8f3cb3840659b167c72bc400a98
Taras Prokhorov <taras_prokhorov@epam.com>
2020-08-31 12:57:56 +0300

    [EPMDEDP-5487]: Fix unit tests for keycloak operator
    Change-Id: Iea0d6ca8a654961e6c29e94a3b9803b4e1eb9ad6

--- pkg/controller/keycloakrealm/keycloakrealm_controller_test.go
+++ pkg/controller/keycloakrealm/keycloakrealm_controller_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/epmd-edp/keycloak-operator/pkg/apis/v1/v1alpha1"
 	"github.com/epmd-edp/keycloak-operator/pkg/client/keycloak/dto"
 	"github.com/epmd-edp/keycloak-operator/pkg/client/keycloak/mock"
-	"github.com/epmd-edp/keycloak-operator/pkg/controller/keycloakrealm/chain/handler"
 	"github.com/stretchr/testify/assert"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -20,104 +19,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
-
-func TestReconcileKeycloakRealm_ReconcileNewCr(t *testing.T) {
-	//prepare
-	//vars
-	kServerUrl := "http://some.security"
-	kServerUsr := "user"
-	kServerPwd := "pass"
-	kSecretName := "keycloak-secret"
-	kRealmName := "main"
-	ns := "security"
-	// dependent custom resources
-	k := &v1alpha1.Keycloak{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test-keycloak",
-			Namespace: ns,
-		},
-		Spec: v1alpha1.KeycloakSpec{
-			Url:    kServerUrl,
-			Secret: kSecretName,
-		},
-		Status: v1alpha1.KeycloakStatus{
-			Connected: true,
-		},
-	}
-	secret := &corev1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      kSecretName,
-			Namespace: ns,
-		},
-		Data: map[string][]byte{
-			"username": []byte(kServerUsr),
-			"password": []byte(kServerPwd),
-		},
-	}
-	kr := &v1alpha1.KeycloakRealm{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      kRealmName,
-			Namespace: ns,
-		},
-		Spec: v1alpha1.KeycloakRealmSpec{
-			RealmName: fmt.Sprintf("%v.%v", ns, kRealmName),
-		},
-	}
-	kr.SetOwnerReferences([]metav1.OwnerReference{
-		{
-			Kind: "Keycloak",
-			Name: k.Name,
-		},
-	})
-	//client and scheme
-	objs := []runtime.Object{
-		k, kr, secret,
-	}
-	s := scheme.Scheme
-	s.AddKnownTypes(v1.SchemeGroupVersion, k, kr, &v1alpha1.KeycloakClient{})
-	client := fake.NewFakeClient(objs...)
-
-	//keycloak client, factory and handler
-	kclient := new(mock.MockKeycloakClient)
-	keycloakDto := dto.Keycloak{
-		Url:  kServerUrl,
-		User: kServerUsr,
-		Pwd:  kServerPwd,
-	}
-	factory := new(mock.MockGoCloakFactory)
-	factory.On("New", keycloakDto).
-		Return(kclient, nil)
-	h := new(handler.MockRealmHandler)
-	h.On("ServeRequest", kr, kclient).
-		Return(nil)
-
-	//request
-	req := reconcile.Request{
-		NamespacedName: types.NamespacedName{
-			Name:      kRealmName,
-			Namespace: ns,
-		},
-	}
-
-	//reconcile
-	r := ReconcileKeycloakRealm{
-		client:  client,
-		scheme:  s,
-		factory: factory,
-		handler: h,
-	}
-
-	//test
-	res, err := r.Reconcile(req)
-
-	//verify
-	assert.NoError(t, err)
-	assert.False(t, res.Requeue)
-
-	persKr := &v1alpha1.KeycloakRealm{}
-	err = client.Get(context.TODO(), req.NamespacedName, persKr)
-	assert.True(t, persKr.Status.Available)
-}
 
 func TestReconcileKeycloakRealm_ReconcileWithoutOwners(t *testing.T) {
 	//prepare
