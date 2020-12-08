74ce614cd2a22c9eb82e4056408a58081b13bb9e
Pavlo Yemelianov <pavlo_yemelianov@epam.com>
2020-12-08 16:30:23 +0200

    [EPMDEDP-5778] Extend KeycloakRealm API to specify keycloak owner in the spec
    Change-Id: If275b04edeac4abd3ad8e0188cf9b1a6a07802b4

--- pkg/controller/keycloakrealm/keycloakrealm_controller_test.go
+++ pkg/controller/keycloakrealm/keycloakrealm_controller_test.go
@@ -282,3 +282,91 @@ func TestReconcileKeycloakRealm_ReconcileInvalidOwnerCredentials(t *testing.T) {
 	err = client.Get(context.TODO(), req.NamespacedName, persKr)
 	assert.False(t, persKr.Status.Available)
 }
+
+func TestReconcileKeycloakRealm_ReconcileWithKeycloakOwnerAndInvalidCreds(t *testing.T) {
+	//prepare
+	//vars
+	kServerUrl := "http://some.security"
+	kServerUsr := "user"
+	kServerPwd := "pass"
+	kSecretName := "keycloak-secret"
+	kRealmName := "main"
+	ns := "security"
+	// dependent custom resources
+	k := &v1alpha1.Keycloak{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-keycloak",
+			Namespace: ns,
+		},
+		Spec: v1alpha1.KeycloakSpec{
+			Url:    kServerUrl,
+			Secret: kSecretName,
+		},
+		Status: v1alpha1.KeycloakStatus{
+			Connected: true,
+		},
+	}
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      kSecretName,
+			Namespace: ns,
+		},
+		Data: map[string][]byte{
+			"username": []byte(kServerUsr),
+			"password": []byte(kServerPwd),
+		},
+	}
+	kr := &v1alpha1.KeycloakRealm{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      kRealmName,
+			Namespace: ns,
+		},
+		Spec: v1alpha1.KeycloakRealmSpec{
+			KeycloakOwner: k.Name,
+			RealmName:     fmt.Sprintf("%v.%v", ns, kRealmName),
+		},
+	}
+	//client and scheme
+	objs := []runtime.Object{
+		k, kr, secret,
+	}
+	s := scheme.Scheme
+	s.AddKnownTypes(v1.SchemeGroupVersion, k, kr, &v1alpha1.KeycloakClient{})
+	client := fake.NewFakeClient(objs...)
+
+	//keycloak client, factory and handler
+	keycloakDto := dto.Keycloak{
+		Url:  kServerUrl,
+		User: kServerUsr,
+		Pwd:  kServerPwd,
+	}
+	factory := new(mock.MockGoCloakFactory)
+	factory.On("New", keycloakDto).
+		Return(nil, errors.New("invalid credentials"))
+
+	//request
+	req := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      kRealmName,
+			Namespace: ns,
+		},
+	}
+
+	//reconcile
+	r := ReconcileKeycloakRealm{
+		client:  client,
+		scheme:  s,
+		factory: factory,
+	}
+
+	//test
+	res, err := r.Reconcile(req)
+
+	//verify
+	assert.Error(t, err)
+	assert.False(t, res.Requeue)
+
+	persKr := &v1alpha1.KeycloakRealm{}
+	err = client.Get(context.TODO(), req.NamespacedName, persKr)
+	assert.False(t, persKr.Status.Available)
+}
