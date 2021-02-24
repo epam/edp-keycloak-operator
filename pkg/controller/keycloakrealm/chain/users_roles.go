67a5141afd45620fea89aa6e9c39751cc065b4b1
Mykhailo Bolsunovskyi <mykhailo_bolsunovskyi@epam.com>
2021-02-24 17:17:03 +0200

    [EPMDEDP-6026]: fix no sso realm users and roles creation
    Change-Id: Ib81185fa73fa8043763ea325cad1a747ea55c48d

--- pkg/controller/keycloakrealm/chain/users_roles.go
+++ pkg/controller/keycloakrealm/chain/users_roles.go
@@ -5,6 +5,7 @@ import (
 	"github.com/epmd-edp/keycloak-operator/pkg/client/keycloak"
 	"github.com/epmd-edp/keycloak-operator/pkg/client/keycloak/dto"
 	"github.com/epmd-edp/keycloak-operator/pkg/controller/keycloakrealm/chain/handler"
+	"github.com/pkg/errors"
 )
 
 type PutUsersRoles struct {
@@ -15,13 +16,9 @@ func (h PutUsersRoles) ServeRequest(realm *v1alpha1.KeycloakRealm, kClient keycl
 	rLog := log.WithValues("keycloak users", realm.Spec.Users)
 	rLog.Info("Start putting roles to users")
 	rDto := dto.ConvertSpecToRealm(realm.Spec)
-	if !rDto.SsoRealmEnabled {
-		rLog.Info("sso realm disabled, skip putting roles to users")
-		return nextServeOrNil(h.next, realm, kClient)
-	}
 	err := putRolesToUsers(rDto, kClient)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "error during putRolesToUsers")
 	}
 	rLog.Info("End put role to users")
 	return nextServeOrNil(h.next, realm, kClient)
@@ -31,7 +28,7 @@ func putRolesToUsers(realm dto.Realm, kClient keycloak.Client) error {
 	for _, user := range realm.Users {
 		err := putRolesToOneUser(realm, user, kClient)
 		if err != nil {
-			return err
+			return errors.Wrap(err, "error during putRolesToOneUser")
 		}
 	}
 	return nil
@@ -41,20 +38,30 @@ func putRolesToOneUser(realm dto.Realm, user dto.User, kClient keycloak.Client) 
 	for _, role := range user.RealmRoles {
 		err := putOneRoleToOneUser(realm, user, role, kClient)
 		if err != nil {
-			return err
+			return errors.Wrap(err, "error during putOneRoleToOneUser")
 		}
 	}
 	return nil
 }
 
 func putOneRoleToOneUser(realm dto.Realm, user dto.User, role string, kClient keycloak.Client) error {
-	exist, err := kClient.HasUserClientRole(realm.SsoRealmName, realm.Name, user, role)
+	realmName := realm.Name
+	if realm.SsoRealmEnabled {
+		realmName = realm.SsoRealmName
+	}
+
+	exist, err := kClient.HasUserClientRole(realmName, realm.Name, user, role)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "error during check of client role")
 	}
 	if *exist {
 		log.Info("Role already exists", "user", user, "role", role)
 		return nil
 	}
-	return kClient.AddClientRoleToUser(realm.SsoRealmName, realm.Name, user, role)
+
+	if err := kClient.AddClientRoleToUser(realmName, realm.Name, user, role); err != nil {
+		return errors.Wrap(err, "unable to add client role to user")
+	}
+
+	return nil
 }
