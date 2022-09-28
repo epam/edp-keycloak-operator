7548204e3a91154af6848b69db4688e2b2f19c5e
Zorian Motso <zorian_motso@epam.com>
2022-09-28 18:13:22 +0300

    [EPMDEDP-10648]: fix: Metric ports default value
    Change-Id: Ifdb6a940674bb9217ccdfe743c434c6681fa25c0

--- main.go
+++ main.go
@@ -57,8 +57,8 @@ func main() {
 		enableLeaderElection bool
 	)
 
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8090", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8091", "The address the probe endpoint binds to.")
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
