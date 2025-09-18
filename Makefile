epam/edp-perf-operator

[EPMDEDP-8227]: chore: Update changelog
Signed-off-by: Sergiy Kulanov <sergiy_kulanov@epam.com>
Change-Id: Ie204b50bbe2a89d82f45b0de3f96be571db4ef92

Makefile
@@ -48,3 +48,8 @@ build: clean ## build operator's binary
 .PHONY: clean
 clean:  ## clean up
 	-rm -rf ${DIST_DIR}
+
+# use https://github.com/git-chglog/git-chglog/
+.PHONY: changelog
+changelog: ## generate changelog
+	@git-chglog -o CHANGELOG.md v2.7.0..

epam/edp-keycloak-operator

Change-Id: Ibdeda1f53004c5874bfecd18731d3b1a54cf4c2d

Makefile
@@ -48,3 +48,8 @@ build: clean ## build operator's binary
 .PHONY: clean
 clean:  ## clean up
 	-rm -rf ${DIST_DIR}
+
+# use https://github.com/git-chglog/git-chglog/
+.PHONY: changelog
+changelog: ## generate changelog
+	@git-chglog -o CHANGELOG.md v1.7.0..